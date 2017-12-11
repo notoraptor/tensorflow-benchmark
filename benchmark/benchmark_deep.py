fdb743aceb7761e85ba08059d68410956bc288ea
notoraptor <stevenbocco@gmail.com>
2017-12-11 08:35:16 -0500

    Add handling for 1 or 2 GPUs

--- benchmark/benchmark_deep.py
+++ benchmark/benchmark_deep.py
@@ -23,33 +23,14 @@ def conv2d(x, W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-
-if __name__ == '__main__':
-    np.random.seed(12345678)
-    tf.set_random_seed(87654321)
-
-    parser = argparse.ArgumentParser()
-    default_dtype = 'float32'
-    default_nbatch = 100
-    default_nin = 64
-    default_nout = 10
-    default_nsteps = 1000
-    parser.add_argument("--dtype", type=str, default=default_dtype, help='Input and output dtype (default %s)' % default_dtype)
-    parser.add_argument("--nbatch", type=int, default=default_nbatch, help='Batch size of the layer (default %d)' % default_nbatch)
-    parser.add_argument("--nin", type=int, default=default_nin,
-                        help='Input size (size x size) of the layer, should be a multiple of 4 (default %d)' % default_nin)
-    parser.add_argument("--nout", type=int, default=default_nout, help='Output size of the layer (default %d)' % default_nout)
-    parser.add_argument("--nsteps", type=int, default=default_nsteps, help='Number of training steps (default %d)' % default_nsteps)
-    args = parser.parse_args()
-
+def build_model(args):
     assert args.nin % 4 == 0, "Input size must be a multiple of 4."
 
     length = args.nin
-    image_size = length * length
 
     data = np.random.normal(size=(args.nbatch, length, length, 1)).astype(args.dtype)
     target = np.zeros((args.nbatch, args.nout), dtype=args.dtype)
-    target[np.arange(args.nbatch), np.random.randint(0, args.nout, args.nbatch)] = 1
+    target[np.arange(args.nbatch), np.random.randint(low=0, high=args.nout, size=args.nbatch)] = 1
 
     x_image = tf.constant(data, args.dtype, [args.nbatch, length, length, 1], verify_shape=True)
     y_ = tf.constant(target, args.dtype, [args.nbatch, args.nout], verify_shape=True)
@@ -85,18 +66,59 @@ if __name__ == '__main__':
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-    with tf.Session() as sess:
-        # Start profiling
-        time_start = datetime.now()
-        sess.run(tf.global_variables_initializer())
-        for i in range(args.nsteps):
-            sess.run(train_step)
-            if (i + 1) % 100 == 0:
-                print("Step %d/%d" % (i + 1, args.nsteps))
-        # end profiling
-        time_end = datetime.now()
-        if args.nsteps % 100 != 0:
-            print('End (%d steps)' % args.nsteps)
-        time_spent = time_end - time_start
-        seconds = time_spent.seconds + time_spent.days * 24 * 3600
-        print('Execution time:', seconds, 'sec +', time_spent.microseconds, 'microsec')
+    return train_step
+
+if __name__ == '__main__':
+    np.random.seed(12345678)
+    tf.set_random_seed(87654321)
+
+    parser = argparse.ArgumentParser()
+    default_dtype = 'float32'
+    default_nbatch = 100
+    default_nin = 64
+    default_nout = 10
+    default_nsteps = 1000
+    default_gpu1 = 0
+    default_gpu2 = -1
+    parser.add_argument("--dtype", type=str, default=default_dtype, help='Input and output dtype (default %s)' % default_dtype)
+    parser.add_argument("--nbatch", type=int, default=default_nbatch, help='Batch size of the layer (default %d)' % default_nbatch)
+    parser.add_argument("--nin", type=int, default=default_nin,
+                        help='Input size (size x size) of the layer, should be a multiple of 4 (default %d)' % default_nin)
+    parser.add_argument("--nout", type=int, default=default_nout, help='Output size of the layer (default %d)' % default_nout)
+    parser.add_argument("--nsteps", type=int, default=default_nsteps, help='Number of training steps (default %d)' % default_nsteps)
+    parser.add_argument("--gpu1", type=int, default=default_gpu1, help='Index of 1st GPU to use (default %d). Pass -1 to deactivate.' % default_gpu1)
+    parser.add_argument("--gpu2", type=int, default=default_gpu2, help='Index of 2nd GPU to use (default %d). Pass -1 to deactivate.' % default_gpu2)
+    parser.add_argument("--log", action='store_true', default=False, help='Log device placement (default false)')
+    args = parser.parse_args()
+
+    computations = []
+    first_device_name = ('/cpu:0' if args.gpu1 < 0 else '/gpu:%d' % args.gpu1)
+    with tf.device(first_device_name):
+        computations += [build_model(args)]
+    if args.gpu2 >= 0:
+        with tf.device('/gpu:%d' % args.gpu2):
+            computations += [build_model(args)]
+
+    if computations:
+        print('Ran %d computations.' % len(computations))
+        print()
+        if args.log:
+            config = tf.ConfigProto(log_device_placement=True)
+        else:
+            config=None
+
+        with tf.Session(config=config) as sess:
+            # Start profiling
+            time_start = datetime.now()
+            sess.run(tf.global_variables_initializer())
+            for i in range(args.nsteps):
+                sess.run(computations)
+                if (i + 1) % 100 == 0:
+                    print("Step %d/%d" % (i + 1, args.nsteps))
+            # end profiling
+            time_end = datetime.now()
+            if args.nsteps % 100 != 0:
+                print('End (%d steps)' % args.nsteps)
+            time_spent = time_end - time_start
+            seconds = time_spent.seconds + time_spent.days * 24 * 3600
+            print('Execution time:', seconds, 'sec +', time_spent.microseconds, 'microsec')
